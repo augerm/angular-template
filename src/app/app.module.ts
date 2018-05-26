464c23bfff97509abd673493641e91bb5da54d07
augerm <michaelauger23@gmail.com>
2018-05-26 14:57:14 -0500

    Initial application structure additions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component'
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '**', component: HomeComponent } // default traffic to homepage
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+  	RouterModule.forRoot(
+  		appRoutes
+  	),
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
