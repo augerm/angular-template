1af65eca55ffcf581a1fee0f153055009c2bcd29
augerm <michaelauger23@gmail.com>
2018-10-13 11:01:28 -0500

    Handle error responses

--- src/app/services/proxy.service.ts
+++ src/app/services/proxy.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { ENDPOINTS } from '../constants/endpoints';
 import { VideoMetaData } from '../models/video-meta-data';
+
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +14,25 @@ export class ProxyService {
   constructor(private http: HttpClient) { }
 
   processVideo(videoMetaData: VideoMetaData) {
-  	return this.http.post(ENDPOINTS.proxyServer.processVideo, videoMetaData, {});
-	    // .pipe(
-	    //   catchError(this.handleError('addHero', hero))
-	    // );
+  	return this.http.post(ENDPOINTS.proxyServer.processVideo, videoMetaData, {})
+	    .pipe(
+	      catchError(this.handleError)
+	    );
   }
+
+  private handleError(error: HttpErrorResponse) {
+	  if (error.error instanceof ErrorEvent) {
+	    // A client-side or network error occurred. Handle it accordingly.
+	    console.error('An error occurred:', error.error.message);
+	  } else {
+	    // The backend returned an unsuccessful response code.
+	    // The response body may contain clues as to what went wrong,
+	    console.error(
+	      `Backend returned code ${error.status}, ` +
+	      `body was: ${error.error}`);
+	  }
+	  // return an observable with a user-facing error message
+	  return throwError(
+	    'Something bad happened; please try again later.');
+	};
 }
